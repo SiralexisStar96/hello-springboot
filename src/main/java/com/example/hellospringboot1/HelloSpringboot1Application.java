6f86143a4f6e3450e8cd4ca8bfd86eb4515d9940
Alejandro Garcia Villar <alejandro.garcia-villar@gft.com>
2022-05-20 12:44:37 +0200

    v1.1

---
+++ src/main/java/com/example/hellospringboot1/HelloSpringboot1Application.java
@@ -0,0 +1,13 @@
+package com.example.hellospringboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringboot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringboot1Application.class, args);
+    }
+
+}
